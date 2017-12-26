4061279bc0f6755decfcce4c1cbd7c8064a51ce4
eghc <eringraceharris@gmail.com>
2017-12-26 02:09:53 +0000

    fix procfile

--- app.js
+++ app.js
@@ -8,5 +8,27 @@ http.createServer(function(request,response){
 }).listen(8080);
 console.log('Server running');
 
+// var express = require('express');
+// var app = express();
+
+// app.set('port', (process.env.PORT || 5000));
+
+// app.use(express.static(__dirname + '/public'));
+
+// // views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+
+// app.get('/', function(request, response) {
+//   router.home(request, response);
+// });
+
+// app.get('/cool', function(request, response) {
+//   response.send(cool());
+// });
+
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
 
 
